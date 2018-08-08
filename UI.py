f4fef067ca2fb00b5e8336f3e2547344932b428d
Ledio Terolli <ledio.terolli@gmail.com>
2018-08-08 17:18:37 -0400

    sprites move

--- UI.py
+++ UI.py
@@ -2,9 +2,14 @@ import pygame
 from pygame import gfxdraw
 from process import get_data
 from pygame_functions import *
+import random
 import sys
+from win32api import GetSystemMetrics
 
 screen = screenSize(1920, 1080, True)
+
+screen_size_x = GetSystemMetrics(0)
+screen_size_y = GetSystemMetrics(1)
 
 edge, new_img, list_obj = get_data("images/p13.jpg")
 scX = new_img.shape[0]
@@ -17,46 +22,75 @@ blue = (0, 0, 255)
 clock = pygame.time.Clock()
 mouse_pos = []
 list_custom = []
-list_black = []
-list_other = []
-
+aster_list = []
+static_list = []
 
 for i in range(len(list_obj)):
     if len(list_obj[i].coor) == 3:
         sprite = makeSprite("images/planet2small.png")
         sprite.move(list_obj[i].center[0], list_obj[i].center[1], True)
+        sprite.x = list_obj[i].center[0]
+        sprite.y = list_obj[i].center[1]
+        # sprite.xspeed = random.randint(-5, 5)
+        sprite.xspeed = 0
+        sprite.yspeed = random.randint(-5, 5)
         sprite.scale = 0.5
         sprite.changeImage(0)
-        list_black.append(sprite)
+        aster_list.append(sprite)
         showSprite(sprite)
-
-        # list_black.append(makeSprite("images/blackhole.png"))
-        # moveSprite(list_black[-1], list_obj[i].center[0], list_obj[i].center[1])
-        # list_black[-1].scale = 0.5
-        # list_black[-1].changeImage(0)
-        # showSprite(list_black[-1])
 
     else:
         sprite = makeSprite("images/blackhole.png")
         sprite.move(list_obj[i].center[0], list_obj[i].center[1], True)
         sprite.scale = 1
         sprite.changeImage(0)
-        list_other.append(sprite)
+        static_list.append(sprite)
         showSprite(sprite)
-
-        # list_other.append(makeSprite("images/planet2small.png"))
-        # moveSprite(list_other[-1], list_obj[i].center[0], list_obj[i].center[1])
-        # list_black[-1].scale = 1
-        # list_black[-1].changeImage(0)
-        # showSprite(list_other[-1])
-
 
 while 1:
     speed = 10
     list_poly = []
     clock.tick(120)
 
+    for i in aster_list:
+        i.x += i.xspeed
+        if i.x > screen_size_x:
+            i.x = 0
+        elif i.x < 0:
+            i.x = screen_size_x
 
+        i.y += i.yspeed
+        if i.y > screen_size_y:
+            i.y = 0
+        elif i.y < 0:
+            i.y = screen_size_y
+
+        moveSprite(i, i.x, i.y)
+
+        # controls
+        if key_press("left"):
+            move(list_obj, 0, 1, speed)
+        elif key_press("right"):
+            move(list_obj, 0, 2, speed)
+        elif key_press("up"):
+            move(list_obj, 0, 3, speed)
+        elif key_press("down"):
+            move(list_obj, 0, 4, speed)
+        elif key_press("space"):
+            list_custom.append(mouse_pos)
+            mouse_pos = []
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            pygame.quit()
+            sys.exit()
+        if mouse_press():
+            a = pygame.mouse.get_pos()
+            if len(mouse_pos) == 0:
+                mouse_pos.append(a)
+            if mouse_pos[-1] != a:
+                mouse_pos.append(a)
+
+    '''
 
     # draw photo polygons
     for i in range(len(list_obj)):
@@ -105,4 +139,4 @@ while 1:
             mouse_pos.append(a)
         if mouse_pos[-1] != a:
             mouse_pos.append(a)
-
+    '''
